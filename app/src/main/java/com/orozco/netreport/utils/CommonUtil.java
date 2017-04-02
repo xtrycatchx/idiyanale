680d58b542e5129f8d3503ef4d471d38ec4c152f
xtrycatchx <sydney@PaulSydney.local>
2017-04-02 23:37:10 +0800

    Added common util class

---
+++ app/src/main/java/com/orozco/netreport/utils/CommonUtil.java
@@ -0,0 +1,12 @@
+package com.orozco.netreport.utils;
+
+/**
+ * Paul Sydney Orozco (@xtrycatchx) on 2/4/17.
+ */
+
+public class CommonUtil {
+
+    public static boolean isNullOrEmpty(String str) {
+        return str == null || str.trim().length() == 0;
+    }
+}
