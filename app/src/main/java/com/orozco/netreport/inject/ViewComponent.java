fa6505e34f8bde15ad838e6b6defc2b7f6f9a01d
ac-opensource <a-ar@outlook.com>
2017-04-06 11:17:28 +0800

    Improve data reporting UX

--- app/src/main/java/com/orozco/netreport/inject/ViewComponent.java
+++ app/src/main/java/com/orozco/netreport/inject/ViewComponent.java
@@ -1,11 +1,9 @@
 package com.orozco.netreport.inject;
 
-import com.orozco.netreport.ui.main.MainView;
-
 import dagger.Subcomponent;
 
 @PerView
 @Subcomponent(modules = ViewModule.class)
 public interface ViewComponent {
-    void inject(MainView mainView);
+
 }
